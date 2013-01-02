8751f2c8bbc38fe094d1e4c308201d643b4adc54
robin <palotai.robin@gmail.com>
2013-01-02 13:21:03 +0100

    Transform: Applicative is enough as execTransform constraint

--- AGL/Internal/Transform.hs
+++ AGL/Internal/Transform.hs
@@ -2,12 +2,12 @@ module AGL.Internal.Transform
   ( execTransform
   ) where
 
+import Control.Applicative
 import Control.Category ((>>>))
-import Control.Monad (liftM, join)
 import Control.Lens
 
-import Data.Default (def)
 import Data.Monoid
+import Data.Traversable
 
 import AGL
 import AGL.Internal.Transform.DrawState
@@ -23,20 +23,21 @@ translate (x, y) = dsX +~ x >>> dsY +~ y
 mulAlpha a = dsAlpha %~ mappend a
 
 -- | Folds transforms along paths of the @Picture@ tree, 
---   executing the supplied monadic function at the leaves and accumulating results.
+--   and traverses the resulting leaves using the supplied function.
 -- 
+-- >>> import Data.Default (def)
 -- >>> let echo t img ds = return ds 
 -- >>> let img = TestImage 10 20 pivotCenter
 -- >>> let ds' = head $ runIdentity $ execTransform def img Blank echo
 -- >>> (ds'^.dsX, ds'^.dsY)
 -- (-5, -10)
-execTransform :: (Monad m, Image a) => DrawState -> Picture a -> (a -> DrawState -> m b) -> m [b]
-execTransform ds pic f = execTransform' ds pic
+execTransform :: (Applicative f, Image a) => DrawState -> Picture a -> (a -> DrawState -> f b) -> f [b]
+execTransform ds pic exec = execTransform' ds pic
   where
   execTransform' ds pic = case pic of
-    Blank           -> return []
-    Pictures ps     -> liftM join $ mapM (execTransform' ds) ps
+    Blank           -> pure []
+    Pictures ps     -> concat <$> traverse (execTransform' ds) ps
     Translate v p   -> execTransform' (translate v ds) p 
-    Draw a          -> liftM return $ f a $ translate (both %~ negate $ imagePivotXY a) ds
+    Draw a          -> sequenceA $ [exec a $ translate (both %~ negate $ imagePivotXY a) ds]
     Transparent a p -> execTransform' (mulAlpha a ds) p
 
